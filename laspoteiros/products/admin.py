032411ac5c4e0ae4295bce1ae154b8b0c681d991
Pablo Calderon <pablocalderon9408@gmail.com>
2022-10-27 17:13:13 -0300

    initial models setup

--- laspoteiros/products/admin.py
+++ laspoteiros/products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from laspoteiros.products.models import Product, ProductCategory, ProductImageVariants
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class ProductImageVariantsInLine(admin.TabularInline):
+    model = ProductImageVariants
+    autocomplete_fields = ["product"]
+    list_display = ['product', "image"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', "price"]
+    search_fields = ["name", "price"]
+    inlines = [ProductImageVariantsInLine]
--- laspoteiros/products/models.py
+++ laspoteiros/products/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+from laspoteiros.utils.models import RestaurantModel
+from slugify import slugify
+
+
+class ProductCategory(RestaurantModel):
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"Product category: {self.name}"
+
+
+class Product(RestaurantModel):
+
+    name = models.CharField(
+        max_length=50
+        )
+
+    slug_name = models.SlugField(max_length=200, unique=True, blank=True)
+
+    description = models.CharField(
+        max_length=500,
+        null=True
+    )
+
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE,
+        related_name="category"
+    )
+
+    main_image = models.FileField(
+        upload_to='users/photos',
+        null=True,
+        blank=True
+        )
+
+    price = models.FloatField(
+        default=0
+        )
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug_name:
+            # Check existence
+            self.slug_name = slugify(self.name, separator="-")
+            if Product.objects.filter(slug_name=self.slug_name).exists():
+                self.slug_name = f"{self.slug_name}-{self.pos_id}"
+        return super().save(*args, **kwargs)
+
+
+class ProductImageVariants(RestaurantModel):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='images'
+        )
+
+    image = models.FileField(
+        upload_to='users/photos',
+        null=True,
+        blank=True
+        )
+
+    def __str__(self) -> str:
+        return f"Variant image: {self.pk}"
